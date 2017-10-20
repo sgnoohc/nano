fe87372d5723d6519519fb133e24236d16cb5209
Philip Chang <sgnoohc@gmail.com>
2017-10-20 07:08:25 -0700

    :bug: fixed stupid bug of return v. continue

--- NanoAOD_ScanChain.C
+++ NanoAOD_ScanChain.C
@@ -23,21 +23,22 @@ void ScanChain(TChain* chain, TString output_name, TString base_optstr, int neve
     // Set output
     // -~-~-~-~-~
     RooUtil::AutoHist hists;
+    hists.fill(0, "Mmumu", 0, 120, 60., 120.);
 
     while (looper.nextEvent())
     {
         if (naod.nMuon < 2)
             continue;
 
-        std::vector<TLorentzVector> muons;
-        for (UInt_t imu = 0; imu < naod.nMuon && imu < 7; ++imu) // The "array" type TTree branches have hard limit on total number of entries
-        {
-            if (!( fabs(naod.Muon_dxy[imu]) < 0.05 )) return;
-            if (!( fabs(naod.Muon_dz[imu]) < 0.1 )) return;
-            if (!( fabs(naod.Muon_ip3d[imu]) < 0.015 )) return;
-            if (!( fabs(naod.Muon_sip3d[imu]) < 4 )) return;
-            if (!( fabs(naod.Muon_pfRelIso03_all[imu]) < 0.06 )) return;
-            if (!( fabs(naod.Muon_ptErr[imu]/naod.Muon_pt[imu]) < 0.2 )) return;
+      std::vector<TLorentzVector> muons;
+      for (UInt_t imu = 0; imu < naod.nMuon && imu < 7; ++imu) // The "array" type TTree branches have hard limit on total number of entries
+      {
+            if (!( fabs(naod.Muon_dxy[imu]) < 0.05 )) continue;
+            if (!( fabs(naod.Muon_dz[imu]) < 0.1 )) continue;
+            if (!( fabs(naod.Muon_ip3d[imu]) < 0.015 )) continue;
+            if (!( fabs(naod.Muon_sip3d[imu]) < 4 )) continue;
+            if (!( fabs(naod.Muon_pfRelIso03_all[imu]) < 0.06 )) continue;
+            if (!( fabs(naod.Muon_ptErr[imu]/naod.Muon_pt[imu]) < 0.2 )) continue;
             TLorentzVector mu;
             mu.SetPtEtaPhiM(naod.Muon_pt[imu], naod.Muon_eta[imu], naod.Muon_phi[imu], 0);
             muons.push_back(mu);
